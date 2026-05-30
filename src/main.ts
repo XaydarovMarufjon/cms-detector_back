4797c611d2e2afd6e0ffe210b6c08943bcc19d4c
Marufjon <marufjon@Marufjons-MacBook-Pro.local>
2026-05-30 20:42:07 +0500

    cache alive subdomains and harden scans

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://10.10.80.31'],
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200', 'http://10.10.80.31'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
